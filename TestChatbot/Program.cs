1066ec4d45a90b5aa77773c90937c707bd1aa543
장원덕 <dnjsejr417@outlook.com>
2025-01-26 19:01:42 +0900

    Add project files.

---
+++ TestChatbot/Program.cs
@@ -0,0 +1,57 @@
+﻿// Import packages
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+using System.ClientModel;
+using System;
+using System.Text;
+using OpenAI.Chat;
+using OpenAI;
+
+var endpoint = new Uri("https://models.inference.ai.azure.com"); // github models를 사용하는 경우 항상 이 uri를 사용하는 듯
+var credential = System.Environment.GetEnvironmentVariable("GITHUB_TOKEN"); // 깃헙에서 발급받은 토큰 환경변수
+var modelId = "gpt-4o";
+var apiKeyCredential = new ApiKeyCredential(credential);
+
+
+var openAIOptions = new OpenAIClientOptions()
+{
+    Endpoint = endpoint
+};
+
+var client = new OpenAIClient(apiKeyCredential, openAIOptions);
+
+var builder = Kernel.CreateBuilder();
+builder.AddOpenAIChatCompletion(modelId, client);
+Kernel kernel = builder.Build();
+
+var chat = kernel.GetRequiredService<IChatCompletionService>();
+
+var history = new ChatHistory(); // 프롬프트로 보임
+history.AddSystemMessage("You are a useful chatbot. If you don't know an answer, say 'I don't know!'. Always reply in a funny way. Use emojis if possible.");
+
+while (true)
+{
+    Console.Write("Q: ");
+    var userQ = Console.ReadLine();
+    if (string.IsNullOrEmpty(userQ))
+    {
+        break;
+    }
+    history.AddUserMessage(userQ);
+
+    var sb = new StringBuilder();
+    var result = chat.GetStreamingChatMessageContentsAsync(history);
+    Console.Write("AI: ");
+    await foreach (var item in result)
+    {
+        sb.Append(item);
+        Console.Write(item.Content);
+    }
+    Console.WriteLine();
+
+    history.AddAssistantMessage(sb.ToString());
+}
